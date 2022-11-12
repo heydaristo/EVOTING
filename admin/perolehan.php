30f9231f68197acca3251886e1b43738b3fa83c8
heydaristo <heydaristo@gmail.com>
2022-11-13 00:47:49 +0700

    hapus semua file

--- admin/perolehan.php
+++
@@ -1,7 +0,0 @@
-<div class="text-right">
-   <button id="save-img" class="button  no-print">Simpan Grafik</button>
-   <button class="button  no-print" onclick="window.print()">Cetak</button>
-</div>
-<div style="font-size:18px;">
-   <canvas id="canvas"></canvas>
-</div>
--- admin/welcome.php
+++
@@ -1,4 +0,0 @@
-<div class="welcome">
-   <h2>Selamat Datang di E - Voting</h2>
-   <p style="font-size:18px;">Anda login sebagai <strong><?php echo $_SESSION['user']; ?></strong> dengan hak akses tak terhingga</p>
-</div>
